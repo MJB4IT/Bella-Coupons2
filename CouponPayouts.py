be904d21ebdfd50f8aef0ee3f1508add922e8321
MJB4IT <36966816+mjb4it@users.noreply.github.com>
2018-03-06 07:54:00 -0500

    Added new file that contains Sales total logic and Commission calculations.

---
+++ CouponPayouts.py
@@ -0,0 +1,20 @@
+
+def calc_sales(disc: float = .05)-> float:
+    """Calculate total sales amount based off amount of discount given on the order"""
+    sales_total = 0.00
+    discount_amount = 27.89
+    order_sale_price = discount_amount / disc
+    sales_total += order_sale_price
+    print(round(sales_total, 2))
+    return sales_total
+
+
+total_payout = 0.00
+def coupon_payouts(coupon, commrate: float = .10)-> float:
+    """Calculate the commission rate"""
+    sales = calc_sales(.05)
+    print(coupon, '$' + str(round(sales * commrate, 2)))
+
+
+coupon_payouts('NJDJK', .10)
+
