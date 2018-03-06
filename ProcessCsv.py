72bdc464eff998e5adac82809f8c3d4887069137
MJB4IT <36966816+MJB4IT@users.noreply.github.com>
2018-03-06 00:30:35 -0500

    Update ProcessCsv.py

--- ProcessCsv.py
+++ ProcessCsv.py
@@ -11,7 +11,7 @@ c_name = ''
 # Read each row from the spreadsheet loaded into csv_purchases
 for cp in csv_purchases:
 
-    if c_name != cp[1]:
+    if c_name != cp[1]:                            # Block ensures that all sales for each coupon are totaled
         print(c_name, '$' + str(round(c_total, 2)))
         c_name = cp[1]
         c_total = 0.00                             # Resets working variable once coupon value changes
@@ -22,7 +22,6 @@ print(c_name, '$' + str(round(c_total, 2)))
 
 # Close the file
 purchases.close()
-
-
+#this is the end again and again again
 
 
